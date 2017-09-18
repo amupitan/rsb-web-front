31baa0b057dfdaf876d5fb35279ca278900eb272
michielu <michielu@mac-air-07.cs.iastate.edu>
2017-09-18 17:07:13 -0500

    Fixed failed test

--- src/components/QuickAccess/QuickAccess.test.js
+++ src/components/QuickAccess/QuickAccess.test.js
@@ -4,5 +4,5 @@ import QuickAccess from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<QuickAccess />, div);
+  ReactDOM.render(<QuickAccess content={[]} />, div);
 });
