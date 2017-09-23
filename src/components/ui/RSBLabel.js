e940664f5764060a146cb3e6639a7e9ac75e8e4f
michielu <michielu@mac-air-07.cs.iastate.edu>
2017-09-23 12:49:16 -0500

    Added documentation to RSBLabel

--- src/components/ui/RSBLabel.js
+++ src/components/ui/RSBLabel.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './style/HamburgerMenu.css';
 
+/**
+ * How to use: 
+ * 
+ * Props that are suggested to pass in: 
+ *      name: The text value of the label
+ *      className: Class that the label will use. Make sure to import the file that has that class defined into this file
+ *      onClickFunction: Function you want the button to execute when it is pressed.
+ * 
+ * We can add more later
+ *      
+ * 
+ * USAGE EXAMPLE:
+ * <RSBLabel 
+        name="A RSB Label"
+        className= "danger"
+        onClickFunction={()=>{
+            console.log("Foo");
+        }}
+    />
+ * 
+ */
+
 function RSBLabel(props) {
     return (
         <div>
