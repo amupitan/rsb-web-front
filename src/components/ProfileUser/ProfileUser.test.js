83b0fc14f9a1c728a2fb9addf89f820016726fc3
Victor Amupitan <amupitan@iastate.edu>
2017-09-24 15:45:27 -0500

    Fix ProfileUser test by adding required props

--- src/components/ProfileUser/ProfileUser.test.js
+++ src/components/ProfileUser/ProfileUser.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Profileuser from './';
+import ProfileUser from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Profileuser />, div);
+  ReactDOM.render(<ProfileUser onCloseFunction={()=>{}} />, div);
 }); 
