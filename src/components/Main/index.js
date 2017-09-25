529c88597a86119c24cf69a3b3e747474fdffb55
Victor Amupitan <amupitan@iastate.edu>
2017-09-25 01:38:50 -0500

    Add route to sign up component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../Home'
+import SignUp from '../SignUp'
 import NotFound from '../NotFound'
 
 // The Main component renders one of matching route
@@ -9,6 +10,7 @@ const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Home}/>
+      <Route exact path='/signup' component={SignUp}/>
       <Route path='*' component={NotFound}/>
     </Switch>
   </main>
