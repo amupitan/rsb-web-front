622cf2d6c16fa92abac1e69a4ed1819d72882da3
Victor Amupitan <amupitan@iastate.edu>
2017-09-25 01:53:52 -0500

    Add Route to Login

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,15 +3,17 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from '../Home'
 import SignUp from '../SignUp'
+import Login from '../Login'
 import NotFound from '../NotFound'
 
 // The Main component renders one of matching route
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/signup' component={SignUp}/>
-      <Route path='*' component={NotFound}/>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/signup' component={SignUp} />
+      <Route exact path='/login' component={Login} />
+      <Route path='*' component={NotFound} />
     </Switch>
   </main>
 );
