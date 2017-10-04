1b65cde41223f2de2b68812bf4dc83606b39b222
Walter Seymour <wseymour@mac-air-09.cs.iastate.edu>
2017-10-04 14:49:12 -0500

    functioning google maps api with map markers

--- src/components/MapPage/index.js
+++ src/components/MapPage/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import RSBButton from '../ui/RSBButton';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 import './style.css';
-
-const style = {
-    width: '100%',
-    height: '100%',
-}
 
 export class MapPage extends Component {
     constructor(props) {
@@ -16,18 +13,52 @@ export class MapPage extends Component {
         this.render = this.render.bind(this);
         this.getLocation = this.getLocation.bind(this);
         this.showPosition = this.showPosition.bind(this);
+        this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.onMapClicked = this.onMapClicked.bind(this);
+        this.handleJoinGame = this.handleJoinGame.bind(this);
         this.state = {
-            latitude: 40,
-            longitude: -90
+            latitude: 42.0308,
+            longitude: -93.6319,
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
         }
     }
+
+    handleJoinGame(id){
+        console.log("handle the user joining the game " + id);
+    }
+
+    //handles the event of a game icon being clicked
+    onMarkerClick(props, marker, e) {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+        ReactDOM.render(
+            <button onClick={() => this.handleJoinGame(props.id)} className="btn btn-success">Join Game</button>
+            ,
+            document.getElementById('rsb-map-join-game-window')
+        );
+    }
+
+    //handles the closing of a game option
+    onMapClicked(props) {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    }
+
     showPosition(position) {
         this.setState(() => ({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }));
         console.log(this.state);
-        this.forceUpdate();
     }
     
     getLocation() {
@@ -36,27 +67,40 @@ export class MapPage extends Component {
         } 
     }
 
+    //Uses the google-maps-react library components
     render() {
         return (
             <div className="rsb-main-map">
                 <Map 
-                onReady={this.getLocation()}
                 google={this.props.google} 
-                style={style}
                 zoom={14}
+                clickableIcons={false}
                 initialCenter={{
                     lat: this.state.latitude,
                     lng: this.state.longitude
                 }}
                 
                 >
-
-                    <Marker onClick={this.onMarkerClick}
-                        name={'Current location'} />
-
-                    <InfoWindow onClose={this.onInfoWindowClose}>
+                    <Marker
+                        onClick={this.onMarkerClick}
+                        title={'There is a game here'}
+                        name={'Game 1'}
+                        id={1}
+                        position={{ lat: 42.0308, lng: -93.6319 }} />
+                    <Marker
+                        onClick={this.onMarkerClick}
+                        title={'There is a game here'}
+                        name={'Game 2'}
+                        id={2}
+                        position={{ lat: 42.041421, lng: -93.6438976 }} />
+                    <InfoWindow
+                        marker={this.state.activeMarker}
+                        visible={this.state.showingInfoWindow}>
                         <div>
-                            <h1>Hello</h1>
+                            <h1>{this.state.selectedPlace.name}</h1>
+                            <div id="rsb-map-join-game-window">
+
+                            </div>
                         </div>
                     </InfoWindow>
                 </Map>
@@ -66,6 +110,7 @@ export class MapPage extends Component {
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyABplRWPbn89WsMUko7bMI83SXCiWVTHLY'
+    apiKey: 'AIzaSyABplRWPbn89WsMUko7bMI83SXCiWVTHLY',
+    version: '3.28'
 })(MapPage)
 
