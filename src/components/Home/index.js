a65a648822df090df1ceee147fccc55381e58812
Victor Amupitan <amupitan@iastate.edu>
2017-10-06 02:58:34 -0500

    Make show switch views

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import HamburgerMenu from '../HamburgerMenu';
+import views from './views';
+
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+    }
+
+    this.toggeleMenu = this.toggeleMenu.bind(this);
+  }
+
+  toggeleMenu() {
+    this.setState({
+      showMenu: !this.state.showMenu,
+    })
+  }
 
   render() {
     return (
       <div>
-        <HamburgerMenu/>
+        <HamburgerMenu views={views} onClick={this.toggeleMenu} menu={this.state.showMenu} width={"250px"} />
+        <div className='display'>
+          <Switch>
+            {views.map((view, i) => (
+              <Route exact key={`${view.name}${i}`} path={'/' + view.path} render={props => <view.component {...props} />} />
+            ))}
+          </Switch>
+        </div>
       </div>
     );
   }
 }
+
+
+// Returns a regex representing all the routes
+// An example is /(map|game|friends|)/
+// The reason for the last '|' is to match '/'
+// which is when there's only a slash
+export const appRoutes = (() => {
+  let path = '/(';
+  for (let route of views) {
+    path += route.path + '|'
+  }
+  path += ')/';
+  return path;
+})();
 
 export default Home;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-import Home from '../Home'
+import Home, { appRoutes } from '../Home'
 import SignUp from '../SignUp'
 import Login from '../Login'
-import NotFound from '../NotFound'
+import NotFound from '../NotFound';
+
 
 // The Main component renders one of matching route
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={Home} />
       <Route exact path='/signup' component={SignUp} />
       <Route exact path='/login' component={Login} />
+      <Route exact path={appRoutes} component={Home} />
       <Route path='*' component={NotFound} />
     </Switch>
   </main>
