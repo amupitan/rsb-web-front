d674a5d3a8bc9f4256edc7c270e7f62cc9d21058
Walter Seymour <wseymour@mac-air-09.local>
2017-10-08 01:20:00 -0500

    join page functioning

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import RSBButton from '../ui/RSBButton';
 
 import './style.css';
 
@@ -8,17 +9,53 @@ class Join extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            joinMessage: "Test message",
+            messageType: "text-danger"
         }
 
         //ES6 React.Component doesn't auto bind methods to itself. You need to bind them yourself
         this.render = this.render.bind(this);
+        this.checkCode = this.checkCode.bind(this);
+    }
+
+    checkCode(accessCode){
+        //here we will check the access code and get back a message
+
+        //No game with this code exists.. Please try again.
+        //Sorry, this game is full!
+        //Sorry, you already have a game during this time!
+
+        //You successfully joined {game name}       
+
+        this.setState({
+            joinMessage: "A success or error message may go here",
+            messageType: "text-success"
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>Join Game</h1>
+                <span className="text-center">
+                     <h1>Join Game</h1>
+                </span>
+                <div className="container">
+                    <span className="row">
+                        <span className="col-xs-5 col-xs-offset-3">
+                            <input id="rsb-join-code-input" className="form-control" type="number" placeholder="Enter a game code to join.." />
+                        </span>
+                        <span className="col-xs-1 ">
+                            <RSBButton
+                                text="Submit"
+                                buttonType="success"
+                                onClickFunction={() => (this.checkCode(document.getElementById('rsb-join-code-input').value))}
+                            />
+                        </span>
+                    </span>
+                </div>
+                <div className="text-center">
+                    <span className={this.state.messageType}>{this.state.joinMessage}</span>
+                </div>
             </div>
         )
     }
