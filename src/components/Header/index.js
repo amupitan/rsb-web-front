05ee1cfa19af088e540bdab7b1acc4032cbf7a19
Victor Amupitan <amupitan@iastate.edu>
2017-10-07 22:22:09 -0500

    Add temporary logout component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
+
 // import { Link } from 'react-router-dom'
+import Logout from '../Logout';
+
+import './style.css';
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
   <header>
+    <Logout className={classnames('main-logout')} />
     {/* <nav>
       <ul>
         <li><Link to='/'>Home</Link></li>
