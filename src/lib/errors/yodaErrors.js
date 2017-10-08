7b50bc00150622a8e456de538a35fb26a6a0c7ed
Victor Amupitan <amupitan@iastate.edu>
2017-10-08 01:35:00 -0500

    Add server error formatting

---
+++ src/lib/errors/yodaErrors.js
@@ -0,0 +1,13 @@
+const _invalidUsernamePassword = {
+    'Invalid username': 'The username is invalid',
+    'Passwords do not match': 'The passwords do not match',
+    'Invalid password': 'The password does not match the requirements',
+    'Invalid email': 'The email is invalid',
+    'Invalid username or password': 'The username/password combination was not found',
+}
+
+const _yodaErrors = {
+    7: _invalidUsernamePassword,
+}
+
+export default _yodaErrors;
