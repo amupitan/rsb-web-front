d35e711bb2f88f031ffb9b9d78b1a74c406ebf87
Victor Amupitan <amupitan@iastate.edu>
2017-10-13 12:16:37 -0500

    Add fading effect to notify

---
+++ src/components/ui/Notify/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { wait } from '../../../lib/utils';
+
+import './style.css';
+
+class Notify extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: true,
+        }
+
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClose(evt) {
+        evt.persist();
+        this.setState({
+            display: false,
+        });
+        wait(1000).then(this.props.onClose);
+    }
+
+    render() {
+        const { title, type, message } = this.props;
+        const classes = classnames('notify-alert', type, {
+            'notify-visible': this.state.display,
+            'notify-hidden': !this.state.display,
+        })
+        return (
+            <div className={classes}>
+                <span className="closebtn" onClick={this.handleClose}>&times;</span>
+                <strong>{title && `${title}: `}</strong>{message}
+            </div>
+        );
+    }
+}
+
+Notify.defaultProps = {
+    type: 'info',
+    title: '',
+    message: '',
+};
+
+Notify.propTypes = {
+    title: PropTypes.string,
+    message: PropTypes.string,
+    type: PropTypes.oneOf(['info', 'success', 'warning', 'danger']),
+    onClose: PropTypes.func.isRequired,
+}
+export default Notify;
