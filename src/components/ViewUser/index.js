6ac0170b3e6794d84310893b3eef01990148b124
Michielu Menning <michielu@iastate.edu>
2017-10-14 17:50:45 -0500

    Convert viewUser to stateless component

--- src/components/ViewUser/index.js
+++ src/components/ViewUser/index.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react';
-import {getUser, getHeading, getFriends, getGameHistory}from './controller';
-
+import React from 'react';
+import { getUser, getHeading, getFriends, getGameHistory } from './controller';
 import './style.css';
 
-class ViewUser extends Component {
-    constructor(props) {
-        super(props);
-        this.render = this.render.bind(this);
-        console.log("In here, ", props);
-    }
+function ViewUser(props) {
+    const userInstance = getUser(props.match.params.username);
 
-    render() {
-        const userInstance = getUser(this.props.match.params.username);
-        return (
-            <div className="panel col-xs-10 col-xs-offset-1">
-                {getHeading(userInstance)}
-                <div className="row">
-                    {getFriends(userInstance)}
-                    {getGameHistory(userInstance)}
-                </div>
-            </div >
-        )
-    }
+    return (
+        <div className="panel col-xs-10 col-xs-offset-1">
+            {getHeading(userInstance)}
+            <div className="row">
+                {getFriends(userInstance)}
+                {getGameHistory(userInstance)}
+            </div>
+        </div >
+    )
 }
 
 export default ViewUser
