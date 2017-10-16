b24c728838e6c2080897e45591e92843fa375eaf
Michielu Menning <michielu@iastate.edu>
2017-10-16 14:52:15 -0500

    Add loader to viewUser

--- src/components/ViewUser/index.js
+++ src/components/ViewUser/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {getHeading, getFriends, getGameHistory } from './controller';
 import user from '../../lib/user';
+import { LoaderPage } from '../ui/Loader';
+
 
 
 import './style.css';
@@ -19,18 +21,14 @@ class ViewUser extends Component{
 
     async getUser() {
         const u = await user(this.props.match.params.username);
-        console.log("U: ", u);
         this.setState({
             userObj: u
         })
     }
 
     render(){
-        let returnable;
-
         try{
-             console.log("this.state.userObj: ", this.state.userObj)
-            returnable = (
+            return (
                 <div className="panel col-xs-10 col-xs-offset-1">
                 {getHeading(this.state.userObj)}
                 <div className="row">
@@ -40,10 +38,8 @@ class ViewUser extends Component{
             </div >
             )
         } catch(e){
-            returnable = (<h3>Invalid user or some sh*t like that</h3>)
+            return <LoaderPage />
         }
-    
-        return returnable;
     }
 
 }
