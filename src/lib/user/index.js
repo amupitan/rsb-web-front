bd01334d01034d8d448138e4015898a53bec95b1
Michielu Menning <michielu@iastate.edu>
2017-10-21 21:46:53 -0500

    Combine two yoda calls to one

--- src/lib/user/index.js
+++ src/lib/user/index.js
@@ -1,7 +1,6 @@
-import _getUserInfo, { _name, _getUserFriends, } from './user';
+import _getUserInfo, { _name } from './user';
 
 export const getUser = _getUserInfo;
-export const getUserFriends = _getUserFriends
 export const getName = _name;
 
 export default getUser
--- src/lib/user/user.js
+++ src/lib/user/user.js
@@ -14,18 +14,8 @@ export function _name() {
     return session.getItem('username');
 }
 
-export default async function _getUserInfo(username) {
-    const res = await yoda.post('/user/p/0', (new YodaRequest({}, {
-        username: username,
-    })).toString(), true);
-    if (res.error) {
-        _handleError(res.data);
-    }
-    return res.data;
-}
-
-export async function _getUserFriends(username) {
-    const res = await yoda.post('/user/f/1', (new YodaRequest({}, {
+export default async function _getUserInfo(username, { populate = '0' } = {}) {
+    const res = await yoda.post(`/user/p/${populate}`, (new YodaRequest({}, {
         username: username,
     })).toString(), true);
     if (res.error) {
