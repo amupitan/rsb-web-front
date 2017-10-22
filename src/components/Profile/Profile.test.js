6e4aeed669a2d0acc3b80fc1da15c3d34f507e80
Michielu Menning <michielu@iastate.edu>
2017-10-21 23:12:02 -0500

    Profile test fix

--- src/components/Profile/Profile.test.js
+++ src/components/Profile/Profile.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const props = {
-    match: {
-      params: {
-        username: 'kerno'
-      }
-    }
-  }
-  // ReactDOM.render(<Profile params={props} onCloseFunction={() => { }} />, div);
+  const notify = { show: () => { }, hide: () => { } };
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <Route path='/user/:username' render={(props) => <Profile {...props} notify={notify} onCloseFunction={() => { }} />} />
+    </BrowserRouter>, div);
 }); 
