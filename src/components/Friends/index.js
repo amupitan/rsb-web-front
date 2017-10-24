8b153db00ecd48a7738c964d65d9cfbbcce70b97
Michielu Menning <michielu@iastate.edu>
2017-10-24 00:00:53 -0500

    Enable search to work

--- src/components/Friends/index.js
+++ src/components/Friends/index.js
@@ -17,14 +17,10 @@ class Friends extends Notifiable(Component) {
     constructor(props) {
         super(props);
         this.render = this.render.bind(this);
-        this.renderUsers = this.renderUsers.bind(this);
         this.filterUsers = this.filterUsers.bind(this);
         this.handleFriendSearch = this.handleFriendSearch.bind(this);
-        this.handleRecentSearch = this.handleRecentSearch.bind(this);
         this.state = {
-            //     currentRecents: data.result[0].RecentPlayers,
-            //     friendSearch: "",
-            //     recentSearch: ""
+            friendSearch: ""
         }
     }
 
@@ -34,33 +30,24 @@ class Friends extends Notifiable(Component) {
 
     async getAllFriends() {
         const username = getName();
-        console.log("username: ", username)
         const friends = await getFriends(username);
-        console.log("Friends: ", friends);
         this.setState({
             userFriends: friends
         });
-        console.log("state: ", this.state)
         return friends;
     }
 
-    filterUsers(array, keyword, arrayName) {
+    filterUsers(array, keyword) {
         let newArray = [];
         for (let i = 0; i < array.length; ++i) {
-            if (array[i].Username.toLowerCase().includes(keyword.toLowerCase())) {
+            if (array[i].username.toLowerCase().includes(keyword.toLowerCase())) {
                 newArray.push(array[i]);
             }
         }
-        if (arrayName === 'friends') {
-            this.setState({
-                userFriends: newArray
-            });
-        }
-        if (arrayName === "recents") {
-            this.setState({
-                currentRecents: newArray
-            });
-        }
+        this.setState({
+            userFriends: newArray
+        });
+
     }
 
     renderUsers(array) {
@@ -82,12 +69,6 @@ class Friends extends Notifiable(Component) {
         });
     }
 
-    handleRecentSearch(event) {
-        this.setState({
-            recentSearch: event.target.value
-        });
-    }
-
     render() {
         if (!this.state || !this.state.userFriends) {
             return <LoaderPage />
@@ -101,9 +82,8 @@ class Friends extends Notifiable(Component) {
                         <button type="submit"
                             onClick={
                                 () => {
-                                    this.filterUsers(data.result[0].Friends,
-                                        this.state.friendSearch,
-                                        "friends")
+                                    this.filterUsers(this.state.userFriends,
+                                        this.state.friendSearch)
                                 }
                             }>
                             <span className="glyphicon glyphicon-search"></span>
@@ -115,27 +95,6 @@ class Friends extends Notifiable(Component) {
                         </div>
                     </div>
                 </div>
-                {/* <div className="panel panel-default rsb-recent-players-panel">
-                    <div className="panel-heading text-center">
-                        <h3 className="">Recent Players</h3>
-                        <input className="" type="search" value={this.state.recentSearch} id="rsb-recent-players-search-bar" placeholder="Search Recent.." onChange={this.handleRecentSearch} />
-                        <button type="submit"
-                            onClick={
-                                () => {
-                                    this.filterUsers(data.result[0].RecentPlayers,
-                                        this.state.recentSearch,
-                                        "recents")
-                                }
-                            }>
-                            <span className="glyphicon glyphicon-search"></span>
-                        </button>
-                    </div>
-                    <div className="panel-body">
-                        <div className="row">
-                            {this.renderUsers(this.state.currentRecents)}
-                        </div>
-                    </div> */}
-                {/* </div> */}
             </div >
         )
     }
