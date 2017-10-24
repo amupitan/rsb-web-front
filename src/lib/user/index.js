3c4cba15c4a9ee58815a649c1e157358daeaa19a
Michielu Menning <michielu@iastate.edu>
2017-10-23 23:48:35 -0500

    Add getUserFriends in lib/user

--- src/lib/user/index.js
+++ src/lib/user/index.js
@@ -1,6 +1,7 @@
-import _getUserInfo, { _getLoggedInUserName } from './user';
+import _getUserInfo, { _getUserFriends, _getLoggedInUserName } from './user';
 
 export const getUser = _getUserInfo;
 export const getLoggedInUserName = _getLoggedInUserName;
+export const getFriends = _getUserFriends;
 
 export default getUser
