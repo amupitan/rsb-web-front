a39ff4ba3ca2b413ada6f5d4ce8817e0d5acdf2a
Michielu Menning <michielu@iastate.edu>
2017-10-27 11:28:03 -0500

    Upload file button

--- src/components/Profile/Heading.js
+++ src/components/Profile/Heading.js
@@ -2,12 +2,31 @@ import React from 'react';
 
 import Avatar from '../ui/Avatar';
 
-export default ({ avatar, lastname, firstname, username }) =>  (
+export default ({ avatar, lastname, firstname, username }) => {
+    function _handleImageChange(e) {
+        let reader = new FileReader();
+        let file = e.target.files[0];
+
+        reader.onloadend = () => {
+            console.log("Loaded!");
+            console.log(reader);
+            // this.setState({
+            //     file: file,
+            //     imagePreviewUrl: reader.result
+            // });
+        }
+
+        reader.readAsDataURL(file)
+    }
+    return (
         <div className="rsb-profile-card">
-            <Avatar avatar={avatar} alt='profile-pic' className='rsb-profile-img' />            
+            <Avatar avatar={avatar} alt='profile-pic' className='rsb-profile-img' />
+            <input type="file" id="upload-pic" accept="image/jpeg, image/png" onChange={(e) => _handleImageChange(e)} />
+
             <div className="rsb-profile-info-container">
-                <h4><b>{username}</b></h4> 
-                <p>{`${firstname} ${lastname}`}</p> 
+                <h4><b>{username}</b></h4>
+                <p>{`${firstname} ${lastname}`}</p>
             </div>
-      </div>
-    );
+        </div>
+    )
+}
