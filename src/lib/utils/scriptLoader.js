8879f8be2a806095db6e6f9ef9bb41458f73d91f
Victor Amupitan <amupitan@iastate.edu>
2017-10-29 00:55:35 -0500

    fix console error in test

--- src/lib/utils/scriptLoader.js
+++ src/lib/utils/scriptLoader.js
@@ -3,6 +3,8 @@ import deepFreeze from './deepFreeze';
 export const loadScript = (src, { async = true, name = '' } = {}) => (
     new Promise((resolve, reject) => {
         const ref = window.document.getElementsByTagName("script")[0];
+        if (!ref) return; // happens during test
+
         const script = window.document.createElement("script");
         script.src = src;
         script.async = async;
