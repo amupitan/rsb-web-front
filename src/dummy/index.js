99416f78a4066f7f7fdf0992bf059cbe706058ef
Victor Amupitan <amupitan@iastate.edu>
2017-10-29 00:07:46 -0500

    Remove dummy data

--- src/dummy/index.js
+++ src/dummy/index.js
@@ -1,17 +1,1 @@
-import getMarkers from './mapMarkers';
-import game from './game';
-import games from './games';
-
-export default (url) => {
-    switch (url) {
-        case "games/l/lng/0/lat/0":
-            return getMarkers;
-        case "/game/g/123":
-            return game;
-        case "/game/join/i":
-            return { data: games[0] };
-        default:
-            return {}
-    }
-
-} 
+export default (url) => ({});
