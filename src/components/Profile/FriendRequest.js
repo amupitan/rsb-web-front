0e410dd95ddd208ef07be1d04b2c2e3e85be415f
Victor Amupitan <amupitan@iastate.edu>
2017-10-28 23:40:44 -0500

    Use avatar to render profile pics and optmize sub components

--- src/components/Profile/FriendRequest.js
+++ src/components/Profile/FriendRequest.js
@@ -4,28 +4,17 @@ import UserRequests from './UserRequests';
 
 
 export default ({ friendRequests }) => {
-    if (friendRequests && friendRequests.length > 0) {
-        return (
-            <div className="col-sm-6 panel panel-default">
-                <div className="panel-heading-rsb">
-                    <h2>Friend Requests</h2>
-                </div>
-                <div className="scroll-info panel-body">
-                    <UserRequests
-                        info={friendRequests}
-                    />
-                </div>
-            </div>
-        )
-    }
+    const requests = friendRequests && friendRequests.length > 0 ?
+        <UserRequests requests={friendRequests} /> :
+        <span>No friend requests</span>;
     return (
         <div className="col-sm-6 panel panel-default">
             <div className="panel-heading-rsb">
                 <h2>Friend Requests</h2>
-            </div>
-            <div className="scroll-info panel-body">
-                <span>No friend requests</span>
+                <div className="scroll-info panel-body">
+                    {requests}
+                </div>
             </div>
         </div>
-    )
+    );
 }
--- src/components/Profile/Heading.js
+++ src/components/Profile/Heading.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default ({ profilePic, defaultImg, lastname, firstname, username }) => {
+import Avatar from '../ui/Avatar';
+
+export default ({ avatar, lastname, firstname, username }) => {
     return (
         <div className="row">
             <div className="col-sm-6 text-right">
-                <img src={profilePic || defaultImg} alt="Profile" className="profile-pic" />
+                <Avatar avatar={avatar} alt='profile-pic' className='profile-pic' />
             </div>
             <div className="col-sm-6 text-left">
                 <h4>{username}</h4>
