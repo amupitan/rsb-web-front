12de7154a50d2f22c24b2639270632a651ec3c6f
Victor Amupitan <amupitan@iastate.edu>
2017-11-01 16:59:17 -0500

    Change style of upload profile pic  icon

--- src/components/Profile/Heading.js
+++ src/components/Profile/Heading.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import Avatar from '../ui/Avatar';
 
-export default ({ avatar, lastname, firstname, username, handleImageChange }) => (
+const Heading = ({ avatar, lastname, firstname, username, handleImageChange }) => (
     <div className="rsb-profile-card">
         <Avatar avatar={avatar} alt='profile-pic' className='rsb-profile-img' />
-        <label htmlFor="image-upload" className="upload-pic" >Upload Profile Picture</label>
-        <input type="file" id="image-upload" accept="image/jpeg, image/png" onChange={(e) => handleImageChange(e)} />
+        <div className='rsb-profile-edit-icon'>
+            <label htmlFor="image-upload" className="upload-pic" ><span className="rsb-profile-dot">&#x270E;</span></label>
+            <input type="file" id="image-upload" accept="image/jpeg, image/png" className='rsb-profile-image-upload' onChange={handleImageChange} />
+        </div>
         <div className="rsb-profile-info-container">
             <h4><b>{username}</b></h4>
             <p>{`${firstname} ${lastname}`}</p>
         </div>
     </div>
-)
+);
+
+export default Heading;
