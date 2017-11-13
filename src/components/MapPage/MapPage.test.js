80dcaed1880a3305a53cabe57015ca20a5e16f42
Walter Seymour <wseymour@iastate.edu>
2017-11-13 00:27:26 -0600

    fixed test case for map

--- src/components/MapPage/MapPage.test.js
+++ src/components/MapPage/MapPage.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MapPage from './';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
