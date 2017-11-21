86f75e329de301977e985731491b2e258f6d1349
Michielu Menning <michielu@iastate.edu>
2017-11-21 12:21:50 -0600

    Make 'friends' component capable of selecting friends to invite

--- src/components/Friends/index.js
+++ src/components/Friends/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+
 import { Link } from 'react-router-dom';
 
 import { getFriends, getLoggedInUserName } from '../../lib/user';
+import { sendGameInvite } from '../../lib/game';
 import { Notifiable } from "../../mixins";
 
+import RSBButton from '../ui/RSBButton';
 import { LoaderPage } from '../ui/Loader';
 import Avatar from '../ui/Avatar';
 
@@ -20,6 +24,7 @@ class Friends extends Notifiable(Component) {
 
         this.render = this.render.bind(this);
         this.filterUsers = this.filterUsers.bind(this);
+        this.renderUsers = this.renderUsers.bind(this);
     }
 
     componentDidMount() {
@@ -30,6 +35,9 @@ class Friends extends Notifiable(Component) {
         const username = getLoggedInUserName();
         const friends = await getFriends(username);
         if (!friends.error) {
+            for (let i = 0; i < friends.length; ++i) {
+                friends[`${i}`].selectStatus = false;
+            }
             this.setState({
                 userFriends: friends
             });
@@ -51,18 +59,69 @@ class Friends extends Notifiable(Component) {
         });
     }
 
+    selectFriend(i) {
+        let stateCopy = this.state.userFriends;
+        stateCopy[i].selectStatus = this.state.userFriends[i].selectStatus ? 0 : 1;
+        this.setState({
+            userFriends: stateCopy
+        })
+    }
+
+    getClassSet(user) {
+        let classSet = classNames(
+            'col-xs-3',
+            'text-center',
+            {
+                'user-select': user.selectStatus
+            }
+        )
+        return classSet;
+    }
+
     renderUsers(users) {
-        if (users.length === 0) 
+        if (users.length === 0)
             return <div>No friends with the name '{this.state.friendSearch}' were found</div>
 
-        return users.map((user, i) => (
+        /* `this.props.location.state` is undefined when this component is accessed 
+         * from the hamburger menu, and has `{from: "/game"}` when it gets here from 
+         * trying to invite people from CurrentGame. 
+         */
+        if (this.props.location.state) {
+            return users.map((user, i) => (
+                <div className={this.getClassSet(user)} onClick={() => { this.selectFriend(i) }} key={i}>
+                    <Avatar avatar={user.avatar} alt='profile-pic' className='rsb-friend-icon' />
+                    <span className='rsb-friend-name'>{user.username}</span>
+                </div>
+            ));
+        }
+        else {
+            return users.map((user, i) => (
                 <Link to={`/user/${user.username}`} key={i} >
                     <div className='col-xs-3 text-center'>
                         <Avatar avatar={user.avatar} alt='profile-pic' className='rsb-friend-icon' />
                         <span className='rsb-friend-name'>{user.username}</span>
                     </div>
                 </Link>
-        ));
+            ))
+        }
+    }
+
+    displayInvite() {
+        if (this.props.location.state) {
+            return (
+                <RSBButton
+                    text="Invite"
+                    buttonType="success"
+                    onClickFunction={() => {
+                        for (let i in this.state.userFriends) {
+                            if (this.state.userFriends[i].selectStatus) {
+                                sendGameInvite(this.state.userFriends[i].username)
+                            }
+                        }
+
+                    }}
+                />)
+        }
     }
 
     render() {
@@ -81,6 +140,7 @@ class Friends extends Notifiable(Component) {
                             {/*This either displays the filtered array of friends or all the friends */}
                             {this.renderUsers(this.state.filteredFriends || this.state.userFriends)}
                         </div>
+                        {this.displayInvite()}
                     </div>
                 </div>
             </div >
