b58cb60f21db33cad4fad27d49a96337169ccc5f
Victor Amupitan <amupitan@iastate.edu>
2017-11-22 16:58:04 -0600

    add subscriptions lib

---
+++ src/lib/subscriptions/subscription.js
@@ -0,0 +1,68 @@
+import socket from '../socket';
+
+const socketConnection = socket.connection;
+
+/**
+ * Maintains a connection to a socket connection
+ */
+export default class Subscription {
+    constructor(name, action) {
+        socketConnection.connect();
+        this.subscription = socketConnection.on(name, { listener: action });
+    }
+
+    /**
+     * Returns a new handler
+     * @returns {Subscriber} a new subscription handler
+     */
+    static get subscriber() {
+        return new Subscriber();
+    }
+
+    /**
+     * unsubscribes from the subscription
+     */
+    unsubscribe() {
+        this.subscription.cancel();
+    }
+
+    /**
+     * creates a new subscription
+     * @returns {Subscription} the subscription created
+     */
+    static subscribe = ({ name, action }) => new Subscription(name, action);
+}
+
+/**
+ * Handles a group of {Subscription}s
+ */
+class Subscriber {
+    constructor() {
+        /**
+         * keeps track of the subscriptions
+         */
+        this.subscriptions = [];
+    }
+
+    /**
+     * Subscribes a set of subscriptions to the handler
+     * @param {Array<Subscription>} subscriptions 
+     */
+    multiple(subscriptions = []) {
+        for (const subscription of subscriptions) {
+            this.subscriptions.push(subscription);
+        }
+    }
+
+    /**
+     * unscubscribes from all subscriptions
+     */
+    clearSubscriptions() {
+        for (const subscription of this.subscriptions) {
+            subscription.unsubscribe();
+        }
+        this.subscriptions = [];
+    }
+
+
+}
