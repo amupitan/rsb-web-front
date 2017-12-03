06e3df7f6928b64ed4f254c4df952506c65cbe8d
Michielu Menning <michielu@iastate.edu>
2017-12-02 23:02:53 -0600

    Adding syscriptions

--- src/lib/subscriptions/subscriptions.js
+++ src/lib/subscriptions/subscriptions.js
@@ -1,4 +1,8 @@
 export default {
     RECEIVED_FRIEND_INVITE: 'receivedFriendInvite',
     CANCELLED_FRIEND_INVITE: 'canceledFriendInvite',
+    RESPONSE_FRIEND_INVITE: 'responseFriendInvite',
+    UNFRIEND_USER: 'unfriendUser',
+    REVIEW_GAME_INVITE: 'reviewGameInvite',
+    RECEIVED_GAME_INVITE: 'receivedGameInvite'
 }
